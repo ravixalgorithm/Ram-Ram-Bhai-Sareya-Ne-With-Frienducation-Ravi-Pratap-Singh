ce2eafd08d7a6c82dca3bd59bedc648d995cb91e
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-07 19:06:48 +0530

    p2 sol

--- Day 2/p2_sol.java
+++ Day 2/p2_sol.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
 
+        int buy = Integer.MAX_VALUE,sell=0,profit = 0;
+        
+        for(int i = 0;i<prices.length;i++){
+            buy = Math.min(buy,prices[i]);
+            profit = prices[i]-buy;
+            
+            sell = Math.max(sell,profit);
+        }
+       return sell;
+    }
+}
