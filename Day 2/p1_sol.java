3f4eb011cd19e3fd467b1b0706f1013a49042898
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-01-07 17:47:58 +0530

    Day2

---
+++ Day 2/p1_sol.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int []a = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            a[i]=i;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(a[i]==nums[i]){
+                continue;
+            }
+            else{
+                return a[i];
+            }
+        }
+        return nums.length;
+    }
+}
